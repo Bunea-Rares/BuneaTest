a626611fd2b04c68f4399b14e858696025645506
pete <user.email>
2023-11-21 09:23:45 +0200

    nu mai vreau

--- src/main/java/ro/info/uvt/buneatest/MessageDecoder.java
+++ src/main/java/ro/info/uvt/buneatest/MessageDecoder.java
@@ -1,21 +1,39 @@
 package ro.info.uvt.buneatest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MessageDecoder {
     public String decodeMessage(String encodedMessage, String house) {
-        if ("atreides".equals(house)) {
-            return decodeAtreidesMessage(encodedMessage);
-        } else if ("harkonnen".equals(house)) {
-            return decodeHarkonnenMessage(encodedMessage);
-        } else {
-            return "Unknown house: " + house;
+        StringBuilder decodedMessage = new StringBuilder();
+
+        for (char c : encodedMessage.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int charVal;
+                if("atreides".equals(house)) charVal =  'J' - 'M';
+                else charVal = 'O' - 'M';
+                char decodedChar = (char) (c - charVal);
+                decodedMessage.append(decodedChar);
+            } else {
+                decodedMessage.append(decodeNonLetter(c));
+            }
+        }
+        return decodedMessage.toString();
+    }
+
+    private String decodeNonLetter(char nonLetter) {
+        switch (nonLetter) {
+            case '^':
+                return "a";
+            case '`':
+                return "c";
+            case ')':
+                return ",";
+            case '"', '\u001D':
+                return " ";
+            default:
+                return String.valueOf(nonLetter);
         }
     }
 
-    private String decodeAtreidesMessage(String encodedMessage) {
-        return encodedMessage.replace("Jbppf", "Messi");
-    }
-
-    private String decodeHarkonnenMessage(String encodedMessage) {
-        return encodedMessage.replace("Oguuk", "Messi");
-    }
 }
